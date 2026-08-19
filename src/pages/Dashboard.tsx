0e216b7b1a4483fbb0a5ca8a0cea1b7efbc45936
Vivek Kumar <vkchoudhary050607@gmail.com>
2026-08-19 14:17:43 +0530

    bug

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -13,7 +13,7 @@ import {
 } from 'lucide-react';
 import { User as UserType, Order, Address, Product, Coupon, WebsiteSettings } from '../types';
 import { ForgotPasswordModal } from '../components/ForgotPasswordModal';
-import { SecureOtpWidget } from '../components/SecureOtpWidget';
+import { SecureOtpWidget } from '../components/secureOtpWidget';
 import { sendMSG91Otp, formatMSG91Identifier } from '../services/msg91OtpService';
 import { Pagination } from '../components/Pagination';
 
